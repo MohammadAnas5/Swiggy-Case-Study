09af49b6f754fc4b1ede64624f80d6e6a9666ff3
Mohammad Anas <85899270+MohammadAnas5@users.noreply.github.com>
2023-01-13 15:34:06 +0530

    Update Data_Exploration.sql

--- Data_Exploration.sql
+++ Data_Exploration.sql
@@ -1,1 +1,5 @@
+-- 1 Find customers who have never ordered 
+(SELECT user_id FROM orders) --> All people who orders 
 
+SELECT * FROM users 
+WHERE user_id NOT IN (SELECT user_id FROM orders);  -->Minus from all users
